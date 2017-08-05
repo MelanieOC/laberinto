a923204c0e581d068b3192bc38e54e4852628e2b
MelanieOC <melanie.ocharan@gmail.com>
2017-08-05 12:18:39 -0500

    movimientos

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,8 @@
 var laberinto=document.getElementById('laberinto');
+var izquierda = document.getElementById('izquierda');
+var derecha = document.getElementById('derecha');
+var forward = document.getElementById('forward');
+var abajo = document.getElementById('abajo');
 var mapa=[
 "******************",
 "*_________*______*",
@@ -12,23 +16,76 @@ var mapa=[
 "*o*__*________**W*",
 "******************"];
 
+var x;
+var y;
 
-var tabla = document.createElement('table');
-tabla.setAttribute('cellspacing','0');
-for (var i = 0; i < mapa.length; i++) {
-  var fila = document.createElement('tr');
+var map = [];
+for (var i = 0; i < mapa.length; i++){
+  map[i]=[];
   for (var j = 0; j < mapa[0].length; j++) {
-      var celda = document.createElement('td');
-      if(mapa[i][j]=='*'){
-        celda.style.backgroundColor='black';
-      } else if(mapa[i][j]=='o'){
-        celda.style.backgroundColor='blue';
-      } else if (mapa[i][j]=='W') {
-        celda.style.backgroundColor='red';
-      }
-      fila.appendChild(celda);
+    map[i][j]=mapa[i][j];
   }
-  tabla.appendChild(fila);
 }
 
-laberinto.appendChild(tabla);
+function generarMapa(map) {
+  laberinto.innerHTML='';
+  var tabla = document.createElement('table');
+  tabla.setAttribute('cellspacing','0');
+  for (var i = 0; i < map.length; i++) {
+    var fila = document.createElement('tr');
+    for (var j = 0; j < map[i].length; j++) {
+        var celda = document.createElement('td');
+        if(map[i][j]=='*'){
+          celda.style.backgroundColor='black';
+        } else if(map[i][j]=='o'){
+          x=j;
+          y=i;
+          celda.style.backgroundColor='blue';
+        } else if (map[i][j]=='W') {
+          celda.style.backgroundColor='red';
+        }
+        fila.appendChild(celda);
+    }
+    tabla.appendChild(fila);
+  }
+  laberinto.appendChild(tabla);
+}
+
+generarMapa(map);
+
+
+forward.onclick=function() {
+  if(map[y-1][x]!='*'){
+    map[y][x]='_';
+    y-=1;
+    map[y][x]='o';
+    generarMapa(map);
+  }
+}
+
+derecha.onclick=function() {
+  if(map[y][x+1]!='*'){
+    map[y][x]='_';
+    x+=1;
+    map[y][x]='o';
+    generarMapa(map);
+  }
+}
+
+izquierda.onclick=function() {
+  if(map[y][x-1]!='*'){
+    map[y][x]='_';
+    x-=1;
+    map[y][x]='o';
+    generarMapa(map);
+  }
+}
+
+abajo.onclick=function() {
+  if(map[y+1][x]!='*'){
+    map[y][x]='_';
+    y+=1;
+    map[y][x]='o';
+    generarMapa(map);
+  }
+}
