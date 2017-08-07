8ef75031df1d0d72ee5b0782085c99c294d93b12
MelanieOC <melanie.ocharan@gmail.com>
2017-08-07 15:52:51 -0500

    animacion y mas css

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
 var laberinto=document.getElementById('laberinto');
+var reiniciar = document.getElementById('reiniciar');
 
 var mapa=[
 "******************",
@@ -10,8 +11,8 @@ var mapa=[
 "*_********__**_*_*",
 "*____*______*__*_*",
 "*_**_*__*****_**_*",
-"*o*__*________**_*",
-"****************W*"];
+"*o*__*________**W*",
+"******************"];
 
 
 var x;
@@ -35,27 +36,28 @@ for (var i = 0; i < mapa.length; i++){
     map[i][j]=mapa[i][j];
   }
 }
-var flecha_izquierda='left';
-var flecha_derecha='rigth';
-var flecha_arriba='up';
-var flecha_abajo='down';
+var izquierda='left';
+var derecha='rigth';
+var arriba='up';
+var abajo='down';
 
-function generarMapa(map, flecha) {
+function generarMapa(mapa, direccion) {
   laberinto.innerHTML='';
   var tabla = document.createElement('table');
   tabla.setAttribute('cellspacing','0');
-  for (var i = 0; i < map.length; i++) {
+  tabla.setAttribute('id','mapa');
+  for (var i = 0; i < mapa.length; i++) {
     var fila = document.createElement('tr');
-    for (var j = 0; j < map[i].length; j++) {
+    for (var j = 0; j < mapa[i].length; j++) {
         var celda = document.createElement('td');
-        if(map[i][j]=='*'){
+        if(mapa[i][j]=='*'){
           celda.setAttribute('class', 'muro');
-        } else if(map[i][j]=='o'){
+        } else if(mapa[i][j]=='o'){
           x=j;
           y=i;
-          f=flecha;
-          celda.setAttribute('id',flecha);
-        } else if (map[i][j]=='W') {
+          f=direccion;
+          celda.setAttribute('id',direccion);
+        } else if (mapa[i][j]=='W') {
           xfinal=j;
           yfinal=i;
           celda.setAttribute('class', 'llegada');
@@ -66,20 +68,24 @@ function generarMapa(map, flecha) {
   }
   laberinto.appendChild(tabla);
   if(x==xfinal && y==yfinal){
-    alert('ganaste');
+    var imagen = document.createElement('img');
+    imagen.setAttribute('src','css/manzana.jpg');
+    laberinto.replaceChild(imagen, laberinto.firstChild);
+    var p=document.createElement('p');
+    var texto= document.createTextNode('Yum.. Que rico!');
+    p.appendChild(texto);
+    laberinto.appendChild(p);
   }
 }
 
 generarMapa(map, 'empezar');
 
-function move(a, b, flecha) {
-  if(map[y+a][x+b]!='*'&&f==flecha){
-    map[y][x]='_';
-    y=y+a;
-    x=x+b;
-    map[y][x]='o';
+function move(a, b, direccion) {
+  if(map[y+a][x+b]!='*'&&f==direccion){
+    map[y][x]='x';
+    map[y+a][x+b]='o';
   }
-  generarMapa(map, flecha);
+  generarMapa(map, direccion);
 }
 
 function movimiento(evento)
@@ -87,16 +93,61 @@ function movimiento(evento)
   switch(evento.keyCode)
   {
     case teclas.UP:
-      move(-1, 0, flecha_arriba);
+      move(-1, 0, arriba);
     break;
     case teclas.DOWN:
-      move(1, 0, flecha_abajo);
+      move(1, 0, abajo);
     break;
     case teclas.LEFT:
-      move(0, -1, flecha_izquierda);
+      move(0, -1, izquierda);
     break;
     case teclas.RIGHT:
-      move(0, 1, flecha_derecha);
+      move(0, 1, derecha);
     break;
   }
 }
+
+reiniciar.onclick=function() {
+  map[y][x]='_';
+  map[9][1]='o';
+  generarMapa(map, 'empezar');
+}
+function pegado(a,b, d) {
+ if(d=='arriba'&& d=='abajo'){
+    return map[y+b][x+a]=='*';
+ } else{
+    return map[y-b][x+a]=='*';
+ }
+}
+function check(a, b, flecha1, flecha2) {
+  if(map[y+a][x+b]!='*' && pegado(a,b,f)){
+    move(a, b, f);
+  } else if (map[y+b][x+a]=='*'&& map[y+a][x+b]=='*') {
+    f=flecha1;
+    generarMapa(map,f);
+  } else {
+    f=flecha2;
+    generarMapa(map,f);
+  }
+}
+
+var r = document.getElementById('resolver');
+r.onclick=function resolver(){
+  switch (f) {
+    case arriba:
+      check(-1, 0, derecha, izquierda);
+    break;
+    case derecha:
+      check(0, 1, arriba, abajo);
+    break;
+    case izquierda:
+      check(0, -1, abajo, arriba);
+    break;
+    case abajo:
+      check(1, 0, izquierda, derecha);
+    break;
+    default:
+      f=arriba;
+  }
+};
+
